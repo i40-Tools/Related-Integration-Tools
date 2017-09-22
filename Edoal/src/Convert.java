27724b4e43ac9a962eb772b0ff1dcf7c5968944a
omar rana <omar.rana87@outlook.com>
2017-09-22 03:47:40 +0300

    Adding Edoal autoamtion

---
+++ Edoal/src/Convert.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
+/**
+ * This class uses Edoal Queries to find alignment.
+ * @author omar
+ *
+ */
+
+public class Convert {
+
+	/*
+	 * Preprocess rdf files for two ontologies.
+	 */
+	void preprocessRdf() throws FileNotFoundException, IOException {
+		String content = IOUtils.toString(
+				new FileInputStream(ConfigManager.getFilePath() + "plfile1.ttl"), "UTF-8");
+		content = content.replaceAll("https://w3id.org/i40/aml", "https://w3id.org/i40/aml2");
+		content = content.replaceAll("datetime", "date");
+		IOUtils.write(content, new FileOutputStream(ConfigManager.getFilePath() + "plfile1.ttl"),
+				"UTF-8");
+
+		content = IOUtils.toString(new FileInputStream(ConfigManager.getFilePath() + "plfile0.ttl"),
+				"UTF-8");
+		content = content.replaceAll("datetime", "date");
+		IOUtils.write(content, new FileOutputStream(ConfigManager.getFilePath() + "plfile0.ttl"),
+				"UTF-8");
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		Files2Facts filesAMLInRDF = new Files2Facts();
+		filesAMLInRDF.readFiles(ConfigManager.getFilePath(), ".aml", ".opcua", ".xml");
+		filesAMLInRDF.convertRdf();
+		Similar similar = new Similar();
+		new Convert().preprocessRdf();
+		similar.readFiles(ConfigManager.getFilePath(), ".ttl", ".opcua", ".xml");
+
+		// gets all queries seperated by --
+		String a[];
+		String result = "";
+		try (BufferedReader br = new BufferedReader(new FileReader("query.sparql"))) {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			a = everything.split("--");
+		}
+
+		// gets dataset for query
+		Model modelY = FileManager.get().loadModel(ConfigManager.getFilePath() + "plfile0.ttl");
+		Model modelX = FileManager.get().loadModel(ConfigManager.getFilePath() + "plfile1.ttl");
+
+		Dataset dataset = DatasetFactory.create();
+		dataset.addNamedModel("https://w3id.org/i40/aml2#", modelX);
+		dataset.addNamedModel("https://w3id.org/i40/aml#", modelY);
+
+		// run each query one by one.
+		for (int i = 0; i < a.length; i++) {
+			org.apache.jena.query.QueryExecution qexec = QueryExecutionFactory.create(a[i],
+					dataset.getNamedModel("urn:x-arq:UnionGraph"));
+			Model model = qexec.execConstruct();
+
+			// formatting final result
+			String queryString = "PREFIX ns0:<https://w3id.org/i40/aml/>"
+					+ "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+					+ "PREFIX owl:<http://www.w3.org/2002/07/owl#>"
+					+ "PREFIX ns1:<https://w3id.org/i40/aml2/>"
+					+ "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>"
+					+ "Select * where{ ?x ?y ?z } ";
+
+			try (org.apache.jena.query.QueryExecution qexec1 = QueryExecutionFactory
+					.create(queryString, model)) {
+				org.apache.jena.query.ResultSet results = qexec1.execSelect();
+				for (; results.hasNext();) {
+					org.apache.jena.query.QuerySolution soln = results.nextSolution();
+					org.apache.jena.rdf.model.RDFNode x = soln.get("x");
+					org.apache.jena.rdf.model.Resource r = soln.getResource("z");
+					if (!result.contains("aml1:" + x.asNode().getLocalName() + "," + "aml2:"
+							+ r.getLocalName())) {
+						result += "aml1:" + x.asNode().getLocalName() + "," + "aml2:"
+								+ r.getLocalName() + "\n";
+					}
+				}
+				qexec1.close();
+			}
+		}
+		// final result saved
+		PrintWriter writer = new PrintWriter(ConfigManager.getFilePath() + "edoal.txt", "UTF-8");
+		writer.println(result);
+		writer.close();
+		// convert object to values
+		similar.convertSimilar();
+		System.out.println("Edoal inference Saved in edoal.txt");
+
+	}
+}
---
+++ Edoal/src/Similar.java
@@ -0,0 +1,286 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.util.FileManager;
+
+/**
+ * Converts computed results from object to values by getting there rdf
+ * reference.
+ * 
+ * @author Omar Rana
+ *
+ */
+public class Similar extends Files2Facts {
+
+	private ArrayList<String> duplicateCheck;
+	public static ArrayList<String> amlValues = new ArrayList<String>();
+
+	public Similar() {
+
+	}
+
+	/**
+	 * This function converts computed result into a binary format 1,0. 1
+	 * represent true and 0 represents false. This conversion is required for
+	 * calculation Precision and Recall.
+	 * 
+	 * @throws FileNotFoundException
+	 * 
+	 * @throws IOException
+	 */
+	public void convertSimilar() throws FileNotFoundException {
+		ArrayList<String> aml1List = new ArrayList<String>();
+		ArrayList<String> aml2List = new ArrayList<String>();
+		ArrayList<String> aml1negList = new ArrayList<String>();
+		ArrayList<String> aml2negList = new ArrayList<String>();
+		ArrayList<String> aml1Values = new ArrayList<String>();
+		ArrayList<String> aml2Values = new ArrayList<String>();
+		ArrayList<String> aml1negValues = new ArrayList<String>();
+		ArrayList<String> aml2negValues = new ArrayList<String>();
+		duplicateCheck = new ArrayList<String>();
+
+		try {
+			// Start reading computed result from here
+			try (BufferedReader br = new BufferedReader(
+					new FileReader(new File(ConfigManager.getFilePath() + "edoal.txt")))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					String values[] = line.split(",");
+					if (values.length == 2) {
+						// add values which are true
+						aml1List.add(values[0].replaceAll("aml1:", ""));
+						aml2List.add(values[1].replaceAll("aml2:", ""));
+
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		PrintWriter similar = new PrintWriter(ConfigManager.getFilePath() + "edoal.txt");
+
+		// loop through rdf files and convert Objects of RDF in Values for
+		// better readability for results
+		for (File file : files) {
+			InputStream inputStream = FileManager.get().open(file.getAbsolutePath());
+			model = ModelFactory.createDefaultModel();
+
+			model.read(new InputStreamReader(inputStream), null, "TURTLE");
+
+			// converts object to values
+			if (file.getName().equals("plfile0.ttl")) {
+
+				addAmlValues(aml1List, aml1Values, "aml1:", "hasAttributeName");
+				addAmlValues(aml1List, aml1Values, "aml1:", "refBaseClassPath");
+				addAmlValues(aml1List, aml1Values, "aml1:", "identifier");
+				addAmlValues(aml1List, aml1Values, "aml1:", "hasCorrespondingAttributePath");
+
+			}
+
+			// converts object to values
+			if (file.getName().equals("plfile1.ttl")) {
+				addAmlValues(aml2List, aml2Values, "aml2:", "hasAttributeName");
+				addAmlValues(aml2List, aml2Values, "aml2:", "refBaseClassPath");
+				addAmlValues(aml2List, aml2Values, "aml2:", "identifier");
+				addAmlValues(aml2List, aml2Values, "aml2:", "hasCorrespondingAttributePath");
+			}
+		}
+
+		// update orignal computed results with the new positive values
+		String results = "";
+		for (int j = 0; j < aml1Values.size(); j++) {
+			if (!aml1Values.get(j).equals("aml1:eClassIRDI")
+					&& !aml1Values.get(j).equals("aml1:eClassClassificationClass")
+					&& !aml1Values.get(j).equals("aml1:eClassVersion")) {
+
+				if (!duplicateCheck
+						.contains(aml1Values.get(j) + "\t" + aml2Values.get(j) + "\t" + "1")) {
+					duplicateCheck.add(aml1Values.get(j) + "\t" + aml2Values.get(j) + "\t" + "1");
+
+					results += aml1Values.get(j) + "\t" + aml2Values.get(j) + "\t" + "1" + "\n";
+
+				}
+			}
+		}
+
+		similar.println(results);
+
+		similar.close();
+
+		// Stores aml values in single array
+		// required for integration.
+		for (int i = 0; i < aml1Values.size(); i++) {
+			amlValues.add(aml1Values.get(i).replaceAll("aml1:", ""));
+			amlValues.add(aml2Values.get(i).replaceAll("aml2:", ""));
+		}
+		// removes duplicate
+		amlValues = new ArrayList<String>(new HashSet<String>(amlValues));
+
+	}
+
+	/**
+	 * This function emulates negatives rules results and updated the original
+	 * file. Negative Rules are emulated by take Cartesian product of initial
+	 * seed.
+	 */
+	public void emulateNegativeResults() {
+		try {
+			ArrayList<String> aml1negList = new ArrayList<String>();
+			ArrayList<String> aml2negList = new ArrayList<String>();
+			HashMap<String, String> aml1negValues = new HashMap<String, String>();
+			HashMap<String, String> aml2negValues = new HashMap<String, String>();
+			HashMap<String, String> aml1negpred = new HashMap<String, String>();
+			HashMap<String, String> aml2negpred = new HashMap<String, String>();
+
+			ArrayList<String> otherValues = new ArrayList<String>();
+			ArrayList<String> otherValues2 = new ArrayList<String>();
+
+			// Read all Objects for the cartesian product
+			try (BufferedReader br = new BufferedReader(new FileReader(
+					new File(ConfigManager.getFilePath() + "PSL/test/hasDocument.txt")))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					line = line.trim();
+					if (line.contains("aml1")) {
+						line = line.replaceAll("\t" + "aml1", "");
+						aml1negList.add(line.replaceAll("aml1:", ""));
+					}
+
+					else {
+						line = line.replaceAll("\t" + "aml2", "");
+						aml2negList.add(line.replaceAll("aml2:", ""));
+					}
+
+				}
+
+			}
+
+			// Read all Objects type for the Cartesian product
+			try (BufferedReader br = new BufferedReader(new FileReader(
+					new File(ConfigManager.getFilePath() + "PSL/test/hastype.txt")))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					String values[] = line.split("\t");
+					if (values.length > 1)
+
+						if (line.contains("aml1")) {
+							otherValues.add(values[1].replaceAll("aml1:", ""));
+						}
+
+						else {
+							otherValues2.add(values[1].replaceAll("aml2:", ""));
+						}
+
+				}
+
+			}
+
+			PrintWriter similar = new PrintWriter(new FileOutputStream(
+					new File(ConfigManager.getFilePath() + "PSL/test/similar.txt"), true));
+
+			// Get all rdf object to values reference for all the objects
+			for (File file : files) {
+				InputStream inputStream = FileManager.get().open(file.getAbsolutePath());
+				model = ModelFactory.createDefaultModel();
+
+				model.read(new InputStreamReader(inputStream), null, "TURTLE");
+
+				if (file.getName().equals("plfile0.ttl")) {
+
+					addAmlNegValues(aml1negList, aml1negValues, "aml1:", "hasAttributeName",
+							otherValues, aml1negpred);
+					addAmlNegValues(aml1negList, aml1negValues, "aml1:", "refBaseClassPath",
+							otherValues, aml1negpred);
+					addAmlNegValues(aml1negList, aml1negValues, "aml1:", "identifier", otherValues,
+							aml1negpred);
+				}
+
+				if (file.getName().equals("plfile1.ttl")) {
+					addAmlNegValues(aml2negList, aml2negValues, "aml2:", "hasAttributeName",
+							otherValues2, aml2negpred);
+					addAmlNegValues(aml2negList, aml2negValues, "aml2:", "refBaseClassPath",
+							otherValues2, aml2negpred);
+					addAmlNegValues(aml2negList, aml2negValues, "aml2:", "identifier", otherValues2,
+							aml2negpred);
+				}
+			}
+
+			try {
+				for (String key : aml1negValues.keySet()) {
+					for (String negkey : aml2negValues.keySet()) {
+
+						// predicate and object type should be same for
+						// cartesian product
+						String type1 = aml1negValues.get(key);
+						String type2 = aml2negValues.get(negkey);
+						String pred1 = aml1negpred.get(key);
+						String pred2 = aml2negpred.get(negkey);
+
+						// checks if its not in positive rules
+						if (!duplicateCheck.contains(key + "\t" + negkey + "\t" + "1"))
+
+						{
+							if (type1.equals(type2) && pred1.equals(pred2)) {
+								similar.println(key + "\t" + negkey + "\t" + "0");
+
+							}
+						}
+					}
+
+				}
+			} catch (Exception e) {
+
+			}
+			similar.close();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * This function converts Object into Values reading from Rdf files. The
+	 * values are stored in list.
+	 * 
+	 * @param amlList
+	 * @param amlValue
+	 * @param aml
+	 * @return
+	 */
+	protected ArrayList<String> addAmlValues(ArrayList<?> amlList, ArrayList<String> amlValue,
+			String aml, String predicate) {
+		for (int i = 0; i < amlList.size(); i++) {
+			StmtIterator iterator = model.listStatements();
+			while (iterator.hasNext()) {
+				Statement stmt = iterator.nextStatement();
+				subject = stmt.getSubject();
+				if (subject.asResource().getLocalName().equals(amlList.get(i))) {
+
+					String value = getValue(subject, predicate);
+					if (value != null) {
+						amlValue.add(aml + value);
+						break;
+					}
+				}
+			}
+		}
+		return amlValue;
+	}
+
+}
